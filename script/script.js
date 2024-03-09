d86a1311299e3a4d24acfcc6477cb31ab28707ea
Amna Arif <103307591+Amna113@users.noreply.github.com>
2024-03-09 18:28:54 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,25 @@
+var textWrapper = document.querySelector('.ml14 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false}) // Set loop to false to animate only once
+  .add({
+    targets: '.ml14 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeInOutExpo",
+    duration: 2000 // Increase duration for slower animation
+  }).add({
+    targets: '.ml14 .letter',
+    opacity: [0,1],
+    translateX: [40,0],
+    translateZ: 0,
+    scaleX: [0.3, 1],
+    easing: "easeOutExpo",
+    duration: 2500, // Increase duration for slower animation
+    offset: '-=600',
+    delay: (el, i) => 150 + 25 * i
+  });
+
+
+
+
